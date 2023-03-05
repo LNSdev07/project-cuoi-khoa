69e5134ddff35db647562cd89eb04e50b8061ffb
Lại Ngọc Sơn <107608752+LNSdev07@users.noreply.github.com>
2023-03-05 11:10:55 +0700

    update code
    update code

--- 3.Sourcecode/back-end/ecommerce/src/main/java/com/t3h/ecommerce/repositories/ProductRepository.java
+++ 3.Sourcecode/back-end/ecommerce/src/main/java/com/t3h/ecommerce/repositories/ProductRepository.java
@@ -40,7 +40,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
                              @Param("updatedDateEnd") Long updatedDateEnd);
 
 
+
    @Modifying
    @Query("delete from Product p where p.id in :ids")
    void deleteProduct(@Param("ids") List<Long> ids);
+
+
 }
