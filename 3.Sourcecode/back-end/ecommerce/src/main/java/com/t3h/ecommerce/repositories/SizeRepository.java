4eeecd8a78c4f0e1b32cb14f459f6db9551d38e1
Lại Ngọc Sơn <107608752+LNSdev07@users.noreply.github.com>
2023-03-13 10:52:34 +0700

    feature quan ly size done sonln
    feature quan ly size done sonln

--- 3.Sourcecode/back-end/ecommerce/src/main/java/com/t3h/ecommerce/repositories/SizeRepository.java
+++ 3.Sourcecode/back-end/ecommerce/src/main/java/com/t3h/ecommerce/repositories/SizeRepository.java
@@ -1,11 +1,12 @@
 package com.t3h.ecommerce.repositories;
 
+
 import com.t3h.ecommerce.entities.product.Size;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,20 @@ import java.util.List;
 @Transactional
 public interface SizeRepository extends JpaRepository<Size, Long> {
 
-    @Query("select s from Size s where (lower(s.sizeName) like concat('%', lower(:textSearch),'%' ) or (:textSearch = '')) ")
+    @Query("select s from Size s where s.sizeName like concat('%',:sizeName, '%') and " +
+            "s.sizeCode like concat('%', :sizeCode, '%') and " +
+            "((s.createdDate between :createdDateStart and :createdDateEnd) or :createdDateStart=0) and " +
+            "((s.updatedDate between :updatedDateStart and :updatedDateEnd) or :updatedDateStart=0)")
     Page<Size> findSize(Pageable pageable,
-                        @Param("textSearch") String textSearch);
+                          @Param("sizeName") String colorName,
+                          @Param("sizeCode") String colorCode,
+                          @Param("createdDateStart") Long createdDateStart,
+                          @Param("createdDateEnd") Long createdDateEnd,
+                          @Param("updatedDateStart") Long updatedDateStart,
+                          @Param("updatedDateEnd") Long updatedDateEnd);
+
+
+    @Modifying
+    @Query("delete from Size s where s.id in :ids")
+    void deleteSize(@Param("ids") List<Long> ids);
 }
